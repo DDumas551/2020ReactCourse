ba7a5acf36a79c134573e74b7fd29792ed7c626a
DDumas551 <david.j.dumas@gmail.com>
2020-12-15 10:47:25 -0500

    source code for course

---
+++ jsx/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <div>Hi there!</div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
